9526560007ad3182aafbdf2e7bf6628e42baa8d8
orgx2812 <orgx2812@163.com>
2025-06-18 15:55:57 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -24,4 +24,4 @@ git clone https://github.com/orgx2812/xray-core feeds/packages/net/xray-core
 #sudo rm -rf feeds/packages/net/chinadns-ng
 #sudo rm -rf feeds/luci/applications/luci-app-homeproxy
 sudo rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+git clone https://github.com/orgx2812/golang feeds/packages/lang/golang
